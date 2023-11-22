a97028fa548fd1508549310214712377123e3b2a
thecoderepublicdev <contact@thecoderepublic.dev>
2023-11-22 20:56:14 +0300

    Offer Form Changes

--- components/layout/header/_offer-button.jsx
+++ components/layout/header/_offer-button.jsx
@@ -3,8 +3,11 @@ import { Switch } from '@headlessui/react'
 import { useFormik } from "formik";
 import classNames from "classnames";
 import * as Yup from 'yup';
-import { useHeader } from ".";
 import Modal from '@shared/Modal';
+
+const collectForm = ({...args}) => {
+    console.log({...args});
+}
 
 export default function OfferButton() {
     const [selectedCity, setSelectedCity] = useState(1);
@@ -31,7 +34,7 @@ export default function OfferButton() {
         } 
     }
 
-    const { values, errors, touched, handleSubmit, isSubmitting } = useFormik({
+    const OfferForm = useFormik({
         initialValues: {
             name: "",
             phone: "",
@@ -45,13 +48,16 @@ export default function OfferButton() {
             phone: Yup.string().required('Telefon numarası iletişim için zorunludur.'),
             city: Yup.string().required('İl bilgisi zorunludur'),
             subProvince: Yup.string().required('İlçe bilgisi zorunludur'),
-            owner: Yup.string().required('Bina sahipliği bilgisi zorunludur'),
+            owner: Yup.boolean().required("Bu alan zorunludur").oneOf([true], "Bu alan zorunludur")
         }),
         onSubmit: (values) => {
             console.log(values);
             alert("Form bilgileri console'a gönderildi")
         }
     });
+
+    const { values, errors, touched, handleSubmit, isSubmitting } = OfferForm;
+    
 
     useEffect(function() {
         console.log("Errors: ", errors)
@@ -97,12 +103,12 @@ export default function OfferButton() {
             <Modal.Content>
                 <div className="w-full grid grid-cols-1">
                     <div>
-                        <form className="grid gap-4">
-                            <input required={true} type="text" placeholder="Adınız & Soyadınız" className="p-4  w-full border border-gray-200 outline-none focus:border-[#AA530E] focus:text-[#AA530E] focus:placeholder:text-[#AA530E]"/>
-                            <input required={true} type="tel" placeholder="Telefon Numaranız" className="p-4  w-full border border-gray-200 outline-none focus:border-[#AA530E] focus:text-[#AA530E] focus:placeholder:text-[#AA530E]"/>
+                        <form method="POST" action={false} onSubmit={handleSubmit} className="grid gap-4">
+                            <input onChange={OfferForm.handleChange} required={true} type="text" placeholder="Adınız & Soyadınız" className="p-4  w-full border border-gray-200 outline-none focus:border-[#AA530E] focus:text-[#AA530E] focus:placeholder:text-[#AA530E]"/>
+                            <input onChange={OfferForm.handleChange} required={true} type="tel" placeholder="Telefon Numaranız" className="p-4  w-full border border-gray-200 outline-none focus:border-[#AA530E] focus:text-[#AA530E] focus:placeholder:text-[#AA530E]"/>
                             
                             <div className="grid grid-cols-2 gap-4">
-                                <select disabled={APIErrors.provinces} required={true} onChange={(e) => setSelectedCity(e.target.value)} className="p-4  w-full border border-gray-200 outline-none disabled:bg-gray-200 disabled:text-gray-400 disabled:cursor-not-allowed  focus:border-[#AA530E] focus:text-[#AA530E] focus:placeholder:text-[#AA530E]">
+                                <select disabled={APIErrors.provinces} required={true} onChange={OfferForm.handleChange} className="p-4  w-full border border-gray-200 outline-none disabled:bg-gray-200 disabled:text-gray-400 disabled:cursor-not-allowed  focus:border-[#AA530E] focus:text-[#AA530E] focus:placeholder:text-[#AA530E]">
                                     {provinces?.map((province) => (
                                         <option key={province.id} value={province.id}>
                                             {province.name}
@@ -110,7 +116,7 @@ export default function OfferButton() {
                                     ))}
                                 </select>
 
-                                <select disabled={APIErrors.subProvinces} required={true} className="p-4 w-full border disabled:bg-gray-200 disabled:text-gray-400 disabled:cursor-not-allowed border-gray-200 outline-none  focus:border-[#AA530E] focus:text-[#AA530E] focus:placeholder:text-[#AA530E]">
+                                <select disabled={APIErrors.subProvinces} required={true} onChange={OfferForm.handleChange} className="p-4 w-full border disabled:bg-gray-200 disabled:text-gray-400 disabled:cursor-not-allowed border-gray-200 outline-none  focus:border-[#AA530E] focus:text-[#AA530E] focus:placeholder:text-[#AA530E]">
                                     {subProvinces?.map((subProvince) => (
                                         <option key={subProvince.id}>
                                             {subProvince.name}
@@ -125,7 +131,7 @@ export default function OfferButton() {
                                     id="owner"
                                     required
                                     checked={ownerStatus}
-                                    onChange={setOwnerStatus}
+                                    onChange={OfferForm.handleChange}
                                     className={`${ownerStatus ? 'bg-[#AA530E]' : 'bg-gray-200'}
                                     relative inline-flex h-[34px] w-[70px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
                                 >
@@ -137,7 +143,11 @@ export default function OfferButton() {
                                 </Switch>
                             </div>
 
-                            <textarea className="p-4  w-full border min-h-[150px] resize-none border-gray-200 outline-none  focus:border-[#AA530E] focus:text-[#AA530E] focus:placeholder:text-[#AA530E]" placeholder="Mesajınız"></textarea>
+                            <textarea
+                                onChange={OfferForm.handleChange}
+                                className="p-4  w-full border min-h-[150px] resize-none border-gray-200 outline-none  focus:border-[#AA530E] focus:text-[#AA530E] focus:placeholder:text-[#AA530E]" 
+                                placeholder="Mesajınız">
+                            </textarea>
 
                             <button type="submit" className={classNames(
                                 'animate__animated group p-4 text-center transition-all ease-in-out bg-[#AA530E] hover:bg-black  gap-4 text-white', 
