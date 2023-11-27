357f881573f9bcd12badca327291fbb59b759145
“thecoderepublicdev” <contact@thecoderepublic.dev>
2023-11-27 20:18:44 +0300

    Offer Button için güncellemeler yapıldı.

--- components/layout/header/_offer-button.jsx
+++ components/layout/header/_offer-button.jsx
@@ -78,41 +78,7 @@ export default function OfferButton() {
             className: "px-6 py-4 text-sm transition-all ease-in-out bg-brand-primary hover:bg-black flex items-center gap-4 text-white h-full"
         } 
     }
-
-    const OfferForm = useFormik({
-        initialValues: {
-            fullName: "",
-            phone: "",
-            email: "",
-            city: "",
-            subProvince: "",
-            owner: false,
-            message: ""
-        },
-        validationSchema: Yup.object({
-            fullName: Yup.string().required('Ad & Soyad zorunludur'),
-            phone: Yup.number().required('Telefon numarası iletişim için zorunludur.'),
-            email: Yup.string().email().required('E-Posta Adresi Zorunludur'),
-            city: Yup.string().required('İl bilgisi zorunludur'),
-            subProvince: Yup.string().required('İlçe bilgisi zorunludur'),
-        }),
-        handleSubmit: ({e}) => {
-            console.log("handleSubmit", ...e);
-        },
-        handleChange: ({e}) => {
-            console.log(e);
-        },
-        onSubmit: async (values, { setSubmitting }) => {
-            console.log("onSubmit", values);
-            
-            await axios.post('/api/mail/send', values).then(res => {
-                console.log("Offer Form Sended", res.data)
-                setAPIResponseStatus(res.data.success);
-            })
-        }
-    });
-
-    const { values, errors, touched, handleSubmit, isSubmitting } = OfferForm;
+    
     
     useEffect(function() {
         fetch('https://turkiyeapi.herokuapp.com/api/v1/provinces')
@@ -154,7 +120,7 @@ export default function OfferButton() {
                 owner: false,
                 message: ""
             },
-            validationSchema: Yup.object({
+            validationSchema: Yup.object().shape({
                 fullName: Yup.string().required('Ad & Soyad zorunludur'),
                 phone: Yup.number().required('Telefon numarası iletişim için zorunludur.'),
                 email: Yup.string().email().required('E-Posta Adresi Zorunludur'),
@@ -178,19 +144,22 @@ export default function OfferButton() {
         });
 
         const { values, errors, touched, handleSubmit, isSubmitting } = OfferForm;
+        useEffect(() => {
+            console.log(OfferForm)
+        },[OfferForm])
         return(
             <form method="POST" onSubmit={handleSubmit} className="grid gap-4">
                 <div>
                     <input disabled={isSubmitting} id="fullName" name="fullName" onChange={OfferForm.handleChange} required={true} type="text" placeholder="Adınız & Soyadınız" className="p-4  w-full border border-gray-200 outline-none focus:border-[#AA530E] focus:text-[#AA530E] focus:placeholder:text-[#AA530E]"/>
-                    {errors.fullName && touched.fullName && (<span className="text-red-500 mt-2">{errors.fullName}</span>)}
+                    {touched.fullName && errors.fullName && (<span className="text-red-500 mt-2">{errors.fullName}</span>)}
                 </div>
                 <div>
                     <input disabled={isSubmitting} id="phone" name="phone" onChange={OfferForm.handleChange} required={true} type="tel" placeholder="Telefon Numaranız" className="p-4  w-full border border-gray-200 outline-none focus:border-[#AA530E] focus:text-[#AA530E] focus:placeholder:text-[#AA530E]"/>
-                    {errors.phone && (<span className="text-red-500 mt-2">{errors.phone}</span>)}
+                    {touched.phone && errors.phone && <span className="text-red-500 mt-2">{errors.phone}</span>}
                 </div>
                 <div>
                     <input disabled={isSubmitting} id="email" name="email" onChange={OfferForm.handleChange} required={true} type="email" placeholder="E-Posta Adresiniz" className="p-4  w-full border border-gray-200 outline-none focus:border-[#AA530E] focus:text-[#AA530E] focus:placeholder:text-[#AA530E]"/>
-                    {errors.email && (<span className="text-red-500 mt-2">{errors.email}</span>)}
+                    {touched.email && errors.email && (<span className="text-red-500 mt-2">{errors.email}</span>)}
                 </div>
                 
                 <div className="grid grid-cols-2 gap-4">
@@ -202,7 +171,7 @@ export default function OfferButton() {
                                 </option>
                             ))}
                         </select>
-                        {errors.city && (<span className="text-red-500 mt-2">{errors.city}</span>)}
+                        {touched.city && errors.city && (<span className="text-red-500 mt-2">{errors.city}</span>)}
                     </div>
 
                     <div>
@@ -213,7 +182,7 @@ export default function OfferButton() {
                                 </option>
                             ))}
                         </select>
-                        {errors.subProvince && (<span className="text-red-500 mt-2">{errors.subProvince}</span>)}
+                        {touched.subProvince && errors.subProvince && (<span className="text-red-500 mt-2">{errors.subProvince}</span>)}
                     </div>
                 </div>
 
